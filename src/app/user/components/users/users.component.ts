c62188af2147d71bfd63fd95500ff09b9f683333
saif <saif@tradexa.co.in>
2026-05-07 17:36:44 +0530

    user loaded

--- src/app/user/components/users/users.component.ts
+++ src/app/user/components/users/users.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
+  users: any = [];
+
+  constructor(private userService: UserService) { }
+
+
+
+  ngOnInit() {
+    this.loadUsers()
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe((user) => {
+      this.users = user;
+    })
+  }
 
 }
---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = "https://jsonplaceholder.typicode.com/users";
+
+  constructor(private http: HttpClient) { }
+
+
+  getUsers() {
+    return this.http.get(this.baseUrl);
+  }
+}
